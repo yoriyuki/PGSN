17965779f852a258261ee0ed5adc516f50d7e0c8
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2025-08-16 10:02:02 +0900

    run under pip -e .

--- examples/robot.py
+++ examples/robot.py
@@ -1,12 +1,9 @@
-import sys
-
-import src.pgsn.dsl
-
-sys.path.append("..")
 import pprint
-import pgsn.gsn
 
 from typeguard import install_import_hook
+
+import pgsn.dsl
+import pgsn.gsn
 
 install_import_hook('robot')
 
@@ -40,17 +37,17 @@ s = pgsn.gsn.strategy(description="Argument over each vulnerability",
 pprint.pprint(pgsn.dsl.python_value(pgsn.gsn.evidence(description="Test results").fully_eval()), indent=2)
 
 e1 = pgsn.gsn.evidence(description="Test results")
-pprint.pprint(src.pgsn.dsl.to_python(e1.fully_eval()), indent=2)
+pprint.pprint(pgsn.dsl.to_python(e1.fully_eval()), indent=2)
 
 g1 = pgsn.gsn.goal(description="Vulnerability A is not exploited",
                                                      support=pgsn.gsn.evidence(description="Test results"))
-pprint.pprint(src.pgsn.dsl.to_python(g1.fully_eval()), indent=2)
+pprint.pprint(pgsn.dsl.to_python(g1.fully_eval()), indent=2)
 
-pprint.pprint(src.pgsn.dsl.to_python(s.fully_eval()), indent=2)
-pprint.pprint(src.pgsn.dsl.to_python(robot.fully_eval()), indent=2)
+pprint.pprint(pgsn.dsl.to_python(s.fully_eval()), indent=2)
+pprint.pprint(pgsn.dsl.to_python(robot.fully_eval()), indent=2)
 
 
-pprint.pprint(src.pgsn.dsl.to_python(robot.fully_eval()), indent=2)
+pprint.pprint(pgsn.dsl.to_python(robot.fully_eval()), indent=2)
 
 web_server = pgsn.gsn.goal(description="The server can deal with DoS attacks on the server",
                                 support=pgsn.gsn.evidence(description="Access restriction"))
@@ -72,7 +69,7 @@ system = pgsn.gsn.goal(description="The robot does not make unintended movements
 
 if __name__ == '__main__':
     system_evaluated = system.fully_eval()
-    pprint.pprint(src.pgsn.dsl.to_python(system_evaluated), indent=2)
+    pprint.pprint(pgsn.dsl.to_python(system_evaluated), indent=2)
     # n = gsn.pgsn_to_gsn(system_evaluated, steps=1)
     # js = json.dumps(gsn.python_val(n), sort_keys=True, indent=4)
     # print(js)
--- src/pgsn/gsn.py
+++ src/pgsn/gsn.py
@@ -2,52 +2,52 @@ from attr import attributes
 
 import pgsn.dsl
 
-gsn_class = pgsn.stdlib.define_class(inherit=pgsn.stdlib.base_class,
+gsn_class = pgsn.dsl.define_class(inherit=pgsn.dsl.base_class,
                                      name='GSN_Node',
                                      attributes=["description"])
 
-support_class = pgsn.stdlib.define_class(inherit=gsn_class, name='Support')
+support_class = pgsn.dsl.define_class(inherit=gsn_class, name='Support')
 undeveloped = support_class(description='Undeveloped')
-evidence_class = pgsn.stdlib.define_class(inherit=support_class, name='Evidence')
-strategy_class = pgsn.stdlib.define_class(inherit=support_class, name='Strategy',
+evidence_class = pgsn.dsl.define_class(inherit=support_class, name='Evidence')
+strategy_class = pgsn.dsl.define_class(inherit=support_class, name='Strategy',
                                           attributes=["sub_goals"])
 
-goal_class = pgsn.stdlib.define_class(inherit=gsn_class,
+goal_class = pgsn.dsl.define_class(inherit=gsn_class,
                                       name='Goal',
                                       attributes=["assumptions", "contexts", "support"],
                                       defaults={"assumptions":[], "contexts": [], "support": undeveloped}
                                       )
-assumption_class = pgsn.stdlib.define_class(inherit=gsn_class, name='Assumption')
-context_class = pgsn.stdlib.define_class(inherit=gsn_class, name='Context')
+assumption_class = pgsn.dsl.define_class(inherit=gsn_class, name='Assumption')
+context_class = pgsn.dsl.define_class(inherit=gsn_class, name='Context')
 
-_d = pgsn.stdlib.variable('x')
-_support = pgsn.stdlib.variable('support')
-_assumptions = pgsn.stdlib.variable('assumptions')
-_contexts = pgsn.stdlib.variable('contexts')
-_sub_goals = pgsn.stdlib.variable('sub_goals')
+_d = pgsn.dsl.variable('x')
+_support = pgsn.dsl.variable('support')
+_assumptions = pgsn.dsl.variable('assumptions')
+_contexts = pgsn.dsl.variable('contexts')
+_sub_goals = pgsn.dsl.variable('sub_goals')
 
-evidence = pgsn.stdlib.lambda_abs_keywords(arguments={'description': _d},
+evidence = pgsn.dsl.lambda_abs_keywords(arguments={'description': _d},
                                            body=evidence_class(description=_d))
-strategy = pgsn.stdlib.lambda_abs_keywords(arguments={'description': _d, 'sub_goals': _sub_goals},
+strategy = pgsn.dsl.lambda_abs_keywords(arguments={'description': _d, 'sub_goals': _sub_goals},
                                            body=strategy_class
                                (description=_d, sub_goals=_sub_goals))
-goal = pgsn.stdlib.lambda_abs_keywords(arguments={'description': _d,
+goal = pgsn.dsl.lambda_abs_keywords(arguments={'description': _d,
                                       'assumptions': _assumptions,
                                       'contexts': _contexts,
                                       'support': _support},
-                                       defaults=pgsn.stdlib.record({
-                               'assumptions': pgsn.stdlib.empty,
-                               'contexts': pgsn.stdlib.empty}),
+                                       defaults=pgsn.dsl.record({
+                               'assumptions': pgsn.dsl.empty,
+                               'contexts': pgsn.dsl.empty}),
                                        body=goal_class(description=_d, support=_support))
-assumption = pgsn.stdlib.lambda_abs_keywords(arguments={'description': _d},
+assumption = pgsn.dsl.lambda_abs_keywords(arguments={'description': _d},
                                              body=assumption_class(description=_d))
-context = pgsn.stdlib.lambda_abs_keywords(arguments={'description': _d},
+context = pgsn.dsl.lambda_abs_keywords(arguments={'description': _d},
                                           body=context_class(description=_d))
 
-_goals = pgsn.stdlib.variable('goals')
-immediate = pgsn.stdlib.lambda_abs(_goals, strategy(description="immediate", sub_goals=_goals))
+_goals = pgsn.dsl.variable('goals')
+immediate = pgsn.dsl.lambda_abs(_goals, strategy(description="immediate", sub_goals=_goals))
 
-_evd = pgsn.stdlib.variable('evidence')
-evidence_as_goal = pgsn.stdlib.lambda_abs(_evd, goal(description=_evd('description'), support=_evd))
+_evd = pgsn.dsl.variable('evidence')
+evidence_as_goal = pgsn.dsl.lambda_abs(_evd, goal(description=_evd('description'), support=_evd))
 
 
